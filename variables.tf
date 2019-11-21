ae01fc5f5639beb8917655f050604bf07c65c21e
Ove Danner <ovedanner@gmail.com>
2019-11-21 18:10:10 +0100

    Added variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,46 +1,50 @@
 variable "profile" {
   type = string
   default = "default"
+  description = "The name of the AWS profile to use"
 }
 
 variable "region" {
   type = string
   default = "eu-central-1"
+  description = "The AWS region"
 }
 
 variable "user" {
   type = string
   default = "ec2-user"
+  description = "The user that can provision the nodes"
 }
 
 variable "elasticsearch_folder" {
   type = string
+  description = "The Elasticsearch folder on the node (no trailing slash)"
 }
 
 variable "key_name" {
   type = string
+  description = "The name of the keypair to use for provisioning"
 }
 
 variable "private_key" {
   type = string
+  description = "The local path to the private key used for provisioning"
 }
 
 variable "vpc_id" {
   type = string
+  description = "The ID of the VPC to launch everything in"
 }
 
 variable "vpc_security_group_ids" {
   type = list(string)
   default = null
+  description = "The IDs of the VPC security groups"
 }
 
 variable "ami" {
   type = string
-}
-
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
+  description = "The ID of the AMI to use for the nodes"
 }
 
 # Mapping of node name => properties
@@ -52,4 +56,5 @@ variable "nodes" {
     instance_type = string
     subnet_id = string
   }))
+  description = "An object containing node_name => node_values mappings. Node values are 'instance_type' and 'subnet_id'"
 }
